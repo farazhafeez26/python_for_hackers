4f5c77c8b32a481f313c34aa0eaa7e89427b0da0
unknown <faraz.hafiz@gmail.com>
2024-08-05 21:27:08 +0200

    initial commit

---
+++ python_for_hackers_101.py
@@ -0,0 +1,17 @@
+
+# the python interpreter in kali linux 
+'''python       
+Python 3.11.9 (main, Apr 10 2024, 13:16:36) [GCC 13.2.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 1+1
+2
+>>>  exit () 
+
+man python3
+
+# executing inline python with opening the interprets 
+python3 -c 'print("hello world")'
+
+python3  -c  'import pty; pyt.spawn("/bin/bash");'
+
+'''
