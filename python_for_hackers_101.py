62733077acad63086c50bdeba87993232b86a17b
unknown <faraz.hafiz@gmail.com>
2024-08-06 15:14:59 +0200

    installed sublime editor and configured package settings

--- python_for_hackers_101.py
+++ python_for_hackers_101.py
@@ -15,3 +15,25 @@ python3 -c 'print("hello world")'
 python3  -c  'import pty; pyt.spawn("/bin/bash");'
 
 '''
+'''
+Step-by-Step Guide to Install Sublime Text on Kali Linux:
+
+1. Added the GPG Key:
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+2. Ensured apt is set up to work with HTTPS sources:
+
+sudo apt-get install apt-transport-https
+
+3. Selected the Channel to Use:
+
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+4. Updated apt sources and install Sublime Text:
+
+sudo apt-get update
+sudo apt-get install sublime-text
+
+'''
