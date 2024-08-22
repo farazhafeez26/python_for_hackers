3e246c591a720eaa3514ecf13a529c676ff9af3a
unknown <faraz.hafiz@gmail.com>
2024-08-22 16:31:11 +0200

    22st Aug -loops, reading & wrting files, and Exception handling

--- python_for_hackers_101.py
+++ python_for_hackers_101.py
@@ -1,3 +1,241 @@
+#22nd Aug
+
+#exception error and handling 
+# exeptions can be handled by using try catch block 
+
+
+#arthimetics errors, syntax errors and indentetnation eroors  
+
+try:
+	f = open("Dasads")
+except:
+	print("the junk doesnt exists  ")
+
+
+print("------------")
+
+try:
+	f = open("Dasads") 
+except Exception as e:
+	print(e)
+
+
+print("------------")
+
+
+try:
+	f = open("Dasads") 
+except FileNotFoundError:
+	print("the file does not exist! ")
+except Exception as e:
+	print(e)
+
+
+
+print("------------")
+
+ 
+try:
+	f = open("top100") 
+except FileNotFoundError:   # if there is not filenotfound error then it will go to the next exception 
+ 	print("the file does not exist! ")
+except Exception as e:
+	print(e)
+finally: 
+	print("this message!")
+
+print("----Rasining exceptions--------")
+
+'''n=100 
+if n ==0: 
+	raise Exception("n cant be 0")
+
+print(1/n)	    #it will continue with the calculation
+
+'''
+print("------------")
+
+'''n=0 
+if n ==0: 
+	raise Exception("n cant be 0")
+
+print(1/n)	    #it will not continue with the calculation
+'''
+
+'''
+n="aads"
+
+if type(n) is not int:
+	raise Exception ("n must be an int!") 
+
+print(1/n)  # it could calculate 
+
+'''
+
+#assertion are sanity check  for hard error checking
+
+b=1 	
+assert(b !=0)
+print(1/b)
+
+
+
+
+# read and write file 
+
+f=open ('top100')
+print(f)
+
+f = open ('top100', 'rt')   #rt read text 
+print(f)
+
+#r is read, w is write 
+
+print(f.read())   #print the entire file as a string
+
+print("---------")
+
+f.seek(0)  
+
+print(f.read()) 
+
+
+# print(f.readlines())       #readlines are print array containing each line from the file. 
+
+print(f.readlines()) # you will get [] which means the lines have been read. 
+
+f.seek(0)   # start from 0 bytes
+
+print(f.readlines())
+
+f.seek(0)
+
+for line in f: 
+	print(line.strip()) #remove special characters
+f.close()
+
+
+
+print("---------")
+
+f=open('write.txt', 'w')
+f.write("Testline")
+f.close()
+
+
+f=open('write.txt','a')
+f.write("testline2")
+f.close()
+
+print(f.name)
+print(f.closed)
+print(f.mode)
+
+with open('top100') as f: 
+	for line in f: 
+		print(line)
+
+
+#user input 
+'''
+test = input("Enter the IP:")
+print(test)'''
+
+while True:  #infinite loop
+	test=input("Enter the ip address: ")
+	print(">>> {}".format(test))
+	if test =="exit":
+		break
+	else:
+		print("exploiting..")
+
+
+
+
+
+
+#sets  
+#dont allow duplicate values 
+
+
+print("--------------------")
+set1={"a","b","c"}
+print(type(set1))
+
+set2={"a","a","a"}
+print(set2	)
+
+set3={"a", 0, True}
+set4={"b",1, False}
+
+
+set3.update(set4)   # true will replace 1 because no duplicate values
+
+print(set3)
+
+print(set2)
+
+set1.add("d")
+
+set4.update(lists1) # set4.discard4(4)
+print(set4)
+
+set4.add("j")
+
+set4.remove("a")
+
+print(set4)
+
+#set1.pop()  
+
+#set are ideal when we dont care about the order and the values 
+
+#loops and conditionals
+print("1>=1") if 1>=1 else print ("1 < 1")
+
+if 1>=1:
+	print("1>=1")
+else: 
+	print("1<1")
+
+#nested loop 
+for i in range(3):
+	for j in range(3):
+		print(i,j)
+
+print("---------")
+
+for i in range(5): 
+	if i ==2:
+		break
+	print(i)
+print("---------")
+
+for i in range (5):
+	if i==2: 
+		continue
+	print(i)
+
+
+for i in range (5): 
+	if i==2: 
+		pass 
+	print(i)
+
+
+for c in "string": 
+	print(c)
+
+for key, value in {"a":1, "b":2, "c":3}.items():
+	print(key, value)
+
+for key  in {"a":1, "b":2, "c":3}.keys():
+	print(key)
+
+
+for value  in {"a":1, "b":2, "c":3}.values():
+	print(value)
+
 #21st Aug 
 #bitwise operations
 '''
