31411084266f5bafd0aa56122b46693f64f91426
unknown <faraz.hafiz@gmail.com>
2024-09-03 22:36:36 +0200

    2nd Sept- Sys Module

--- python_for_hackers_101.py
+++ python_for_hackers_101.py
@@ -1,4 +1,71 @@
 
+#3rd Sept - Sys 
+import sys
+
+print (sys.version)
+print (sys.executable)
+print (sys.platform)
+
+'''
+for line in sys.stdin:
+	if line.strip()=="exit": 
+		break
+	sys.stdout.write (">> {}".format(line))
+'''
+
+
+for i in range (1,5):
+	sys.stdout.write(str(i))
+	sys.stdout.flush() 
+
+
+for i in range (1,5):
+	print(i)
+
+
+'''
+import time
+
+for i in range (0,51):
+	time.sleep(0.1)
+	sys.stdout.write("{} [{}{}]".format(i, '#'*i, "."*(50-i)))
+	sys.stdout.flush()
+	sys.stdout.write("\n")
+
+'''
+'''
+print(sys.argv)  #print the name of the script
+
+if len(sys.argv)!=3: 
+	print("[x] To run {} Enter a username and password.format(argv[0])") 
+
+username=sys.argv[1]
+password=sys.argv[2]
+
+print("{} {}".format(username,password))
+
+print(sys.path)
+print(sys.modules)
+
+'''
+
+print(sys.argv)  #print the name of the script
+
+if len(sys.argv)!=3: 
+	print("[x] To run {} Enter a username and password.format(argv[0])") 
+	sys.exit(5)
+
+username=sys.argv[1]
+password=sys.argv[2]
+
+print("{} {}".format(username,password))
+
+sys.exit(0)
+
+#print(sys.path) #python will search for modules to be installed 
+#print(sys.modules) #modulés that are already installed 
+
+
 
 #26th Aug  - Xcomprehensions, Lambdas, functions
 
